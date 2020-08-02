6fa4eed0121378f6dc5b5e4fab8dd31eb69136a9
Adler Coelho Santos <adlercoelhosantos@outlook.com>
2020-08-02 10:00:19 -0300

    Colocando data no vídeo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import BannerMain from './components/BannerMain';
 import Videos from './components/Videos';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <BannerMain />
       <Videos />
+      <Footer />
     </div>
   );
 }
--- src/components/Videos/index.tsx
+++ src/components/Videos/index.tsx
@@ -31,8 +31,9 @@ class Videos extends Component {
                                         src={item.snippet.thumbnails.medium.url}
                                         alt="Thumb do vídeo" >
                                     </img>
-                                    <h2 className="videos-list__wrapper__item-title">{item.snippet.title}</h2>
-                                    <p className="videos-list__wrapper__item-description">
+                                    <span className="videos-list__wrapper__item-date">{this._transformDate(item.snippet.publishedAt)}</span>
+                                    <h2 className="videos-list__wrapper__item-title" title={item.snippet.title}>{item.snippet.title}</h2>
+                                    <p className="videos-list__wrapper__item-description" title={item.snippet.description}>
                                         {item.snippet.description}
                                     </p>
                                 </a>
@@ -44,11 +45,18 @@ class Videos extends Component {
         )
     }
 
-    private _getVideos(): Promise<any> {
+    private async _getVideos(): Promise<any> {
         const key = `AIzaSyA3eoevnwGM7-LMyJvetFRO9kbhmzQpQ9g`;
         const channelId = `UCT4nDeU5pv1XIGySbSK-GgA`;
         const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=${key}&channelId=${channelId}&order=date&maxResults=100`;
-        return fetch(URL).then(json => json.json());
+        return await fetch(URL).then(json => json.json());
+    }
+
+    private _transformDate(publishedAt: string): string {
+        const date = new Date(publishedAt);
+        const day = date.getDate().toString().padStart(2, '0'); 
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        return `${day}/${month}` 
     }
 
 }
